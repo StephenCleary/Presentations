c4820637d697b1aafd2d37573cb95aa3887ab05b
Stephen Cleary <github.ourteddybear@xoxy.net>
2019-08-06 23:23:05 -0500

    Async Streams from ThatConference 2019

---
+++ Async Streams/Code/PagingApiServer/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PagingApiServer.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IReadOnlyCollection<int>> Get(int limit = 10, int offset = 0)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(3));
+            return Enumerable.Range(0, 23).Skip(offset).Take(limit).ToList();
+        }
+    }
+}

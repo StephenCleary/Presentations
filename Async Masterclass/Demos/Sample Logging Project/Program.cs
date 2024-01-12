2efafa74a29a9addbf229a707b0504b63f7610bd
Stephen Cleary <github.ourteddybear@xoxy.net>
2024-01-12 16:52:08 -0500

    2024 CodeMash

--- Async Masterclass/Demos/Sample Logging Project/Program.cs
+++ Async Masterclass/Demos/Sample Logging Project/Program.cs
@@ -1,11 +1,17 @@
 using Log.Services;
+using Serilog;
+using Serilog.ThrowContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddSingleton<WeatherForecastService>();
 
-builder.Services.AddLogging(logging => logging.AddSeq());
+var logger = new LoggerConfiguration()
+	.Enrich.With<ThrowContextEnricher>()
+	.WriteTo.Seq(serverUrl: "http://localhost")
+	.CreateLogger();
+builder.Services.AddLogging(logging => logging.AddSerilog(logger));
 
 builder.Services.AddProblemDetails();
 

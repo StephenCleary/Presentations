02c33f1b17d3114d5e89b4e83283da041b995dd8
Stephen Cleary <900597+StephenCleary@users.noreply.github.com>
2025-03-28 09:26:58 -0700

    Fix location for Win32 service.

--- BackgroundServices/BackgroundServiceDemos/win32service.ps1
+++ BackgroundServices/BackgroundServiceDemos/win32service.ps1
@@ -1,2 +1,2 @@
-New-Service -Name "Demo" -BinaryPathName D:\Code\BackgroundServiceDemos\Demo1\bin\Debug\net9.0\Demo1.exe
+New-Service -Name "Demo" -BinaryPathName D:\Code\Presentations\BackgroundServices\BackgroundServiceDemos\Demo1\bin\Debug\net9.0\Demo1.exe
 Remove-Service -Name "Demo"

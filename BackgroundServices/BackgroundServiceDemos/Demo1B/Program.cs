518414f6d659146f5576865c1cafc426c12a9b92
Stephen Cleary <900597+StephenCleary@users.noreply.github.com>
2025-05-07 18:34:55 -0400

    Add background worker to ASP.NET app.

--- BackgroundServices/BackgroundServiceDemos/Demo1B/Program.cs
+++ BackgroundServices/BackgroundServiceDemos/Demo1B/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+//builder.Services.AddHostedService<Demo2.Worker>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
